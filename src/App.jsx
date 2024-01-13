83fde7bb6983a9db76d0c8dc44488668aa1de668
Beezu Shrestha <beezustha@gmail.com>
2024-01-13 14:14:38 +0545

    Login page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Login from "./pages/Login";
 
 function App() {
 	const [count, setCount] = useState(0);
@@ -10,6 +11,7 @@ function App() {
 		<>
 			<div>
 				<div className="header text-teal-500">Ecommerce website</div>
+				<Login />
 			</div>
 		</>
 	);
